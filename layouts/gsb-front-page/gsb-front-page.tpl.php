cb6a50726e4154a905173756ae2dbbcd6f7325d7
Dumitru Grosul <dumitru.grosul@wearepropeople.com>
2013-02-04 13:22:53 -0800

    Issue: layout for front page added

---
+++ layouts/gsb-front-page/gsb-front-page.tpl.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ * Template for Panopoly Selby Flipped.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+
+<div class="row span12 clear-row panel-display gsb-front-page clearfix <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="span12 row clrear-row front-panel-header-row">
+    <div class="span9 clear-row front-slider-pane">
+      <?php print $content['contentslider']; ?>
+    </div>
+    <div class="span3 front-sidebar-pane">
+      <?php print $content['sidebarnavigation']; ?>
+    </div>
+  </div>
+  <div class="span12 row clrear-row front-panel-feature-row">
+    <div class="span6 clear-row front-feature-one">
+      <?php print $content['featuredcolone']; ?>
+    </div>
+    <div class="span3 clear-row front-panel-feature-two">
+      <?php print $content['featuredcoltwo']; ?>
+    </div>
+    <div class="span3 clear-row front-panel-feature-three">
+      <?php print $content['featuredcolthree']; ?>
+    </div>
+  </div>
+  <div class="span12 row clrear-row front-panel-footer-two">
+    <div class="span6 clear-row front-panel-footer-one">
+      <?php print $content['footercolone']; ?>
+    </div>
+    <div class="span3 clear-row front-panel-footer-two">
+      <?php print $content['footercoltwo']; ?>
+    </div>
+    <div class="span3 clear-row front-panel-footer-three">
+      <?php print $content['footercolthree']; ?>
+    </div>
+  </div>
+</div><!-- /.gsb-front-page -->
