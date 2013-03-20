1810dab93dcae3260dbe172229a4682c19b2305b
kirill_cebotari <kirill.cebotari@wearepropeople.com>
2013-03-20 12:12:37 -0700

    Issue: front page layout fix

--- layouts/gsb-front-page/gsb-front-page.tpl.php
+++ layouts/gsb-front-page/gsb-front-page.tpl.php
@@ -11,33 +11,33 @@
 ?>
 
 <div class="row span12 clear-row panel-display gsb-front-page clearfix <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="span12 row clrear-row front-panel-header-row">
-    <div class="span9 clear-row front-slider-pane">
+  <div class="span12 row clear-row front-panel-header-row">
+    <div class="span9 front-slider-pane">
       <?php print $content['contentslider']; ?>
     </div>
     <div class="span3 front-sidebar-pane">
       <?php print $content['sidebarnavigation']; ?>
     </div>
   </div>
-  <div class="span12 row clrear-row front-panel-feature-row">
-    <div class="span6 clear-row front-feature-one">
+  <div class="span12 row clear-row front-panel-feature-row">
+    <div class="span6  front-feature-one">
       <?php print $content['featuredcolone']; ?>
     </div>
-    <div class="span3 clear-row front-panel-feature-two">
+    <div class="span3 front-panel-feature-two">
       <?php print $content['featuredcoltwo']; ?>
     </div>
-    <div class="span3 clear-row front-panel-feature-three">
+    <div class="span3 front-panel-feature-three">
       <?php print $content['featuredcolthree']; ?>
     </div>
   </div>
-  <div class="span12 row clrear-row front-panel-footer-two">
-    <div class="span6 clear-row front-panel-footer-one">
+  <div class="span12 row clear-row front-panel-footer-two">
+    <div class="span6  front-panel-footer-one">
       <?php print $content['footercolone']; ?>
     </div>
-    <div class="span3 clear-row front-panel-footer-two">
+    <div class="span3 front-panel-footer-two">
       <?php print $content['footercoltwo']; ?>
     </div>
-    <div class="span3 clear-row front-panel-footer-three">
+    <div class="span3 front-panel-footer-three">
       <?php print $content['footercolthree']; ?>
     </div>
   </div>
